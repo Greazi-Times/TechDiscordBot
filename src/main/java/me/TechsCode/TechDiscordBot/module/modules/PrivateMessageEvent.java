68285d3eff5d5b48cbb957f5887db2b9efc4c181
Greazi <info@greazi.com>
2021-10-24 21:44:39 +0200

    Cleaned up  the private message

--- src/main/java/me/TechsCode/TechDiscordBot/module/modules/PrivateMessageEvent.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/modules/PrivateMessageEvent.java
@@ -2,9 +2,11 @@ package me.TechsCode.TechDiscordBot.module.modules;
 
 import me.TechsCode.TechDiscordBot.TechDiscordBot;
 import me.TechsCode.TechDiscordBot.module.Module;
+import me.TechsCode.TechDiscordBot.mysql.Models.DbMember;
+import me.TechsCode.TechDiscordBot.mysql.Models.VerficationQ;
 import me.TechsCode.TechDiscordBot.objects.Requirement;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
-import me.TechsCode.TechDiscordBot.verification.PaypalAPI;
+import me.TechsCode.TechDiscordBot.verification.Verification;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
@@ -15,10 +17,12 @@ public class PrivateMessageEvent extends Module {
 	}
 
 	@Override
-	public void onEnable() {}
+	public void onEnable() {
+	}
 
 	@Override
-	public void onDisable() {}
+	public void onDisable() {
+	}
 
 	@Override
 	public String getName() {
@@ -34,49 +38,58 @@ public class PrivateMessageEvent extends Module {
 	public void OnPrivateMessage(PrivateMessageReceivedEvent e) {
 		String id = e.getAuthor().getId();
 		String message = e.getMessage().getContentRaw();
-		String [] array = message.trim().split(" ");
+		String[] array = message.trim().split(" ");
 
-		if(e.getAuthor().isBot()) return;
-		TechDiscordBot.log(Boolean.toString(PaypalAPI.givenEmail(id)));
+		if (e.getAuthor().isBot()) return;
+		DbMember member = TechDiscordBot.getStorage().retrieveMemberByDiscordId(id);
+		VerficationQ verificationQ = member.getVerificationQ();
 
-		//TODO set up private msg event
-//		if(TechDiscordBot.getStorage().isVerificationQ(id)){
-//			if(array.length==1 && message.contains("why")){
-//				PaypalAPI.privateWhy(e);
-//			} else
-//			if(array.length==1 && message.contains("cancel")){
-//				PaypalAPI.privateCancel(e, id);
-//			} else
-//			if(PaypalAPI.givenEmail(id)){
-//				if(array.length==1 && message.contains("@") && message.contains(".")){
-//					PaypalAPI.privateEmail(e, id, message);
-//				} else {
-//					e.getMessage().replyEmbeds(new TechEmbedBuilder("ERROR").text("Your message does not contain a email address").error().build()).complete();
-//				}
-//			} else
-//			if(array.length==1 && check(message)){
-//				PaypalAPI.privateId(e, id, message);
-//			} else {
-//				e.getMessage().replyEmbeds(new TechEmbedBuilder(TechDiscordBot.getStorage().getSelectedVerificationQ(id) + " Verification").text("Somethings is wrong please use `cancel` to cancel the verification and try again. If the new one fails as well please contact a staff member.").error().build()).complete();
-//
-//			}
-//		}else{
-//			e.getMessage().addReaction("👀").queue();
-//		}
+		if (member.isInVerificationQ()) {
+			if (array.length == 1 && message.contains("why")) {
+				Verification.privateWhy(e);
+
+			} else if (array.length == 1 && message.contains("cancel")) {
+				Verification.privateCancel(e, member);
+
+			} else if (member.getVerificationQ().getEmail().equals("")) {
+				if (array.length == 1 && message.contains("@") && message.contains(".")) {
+					verificationQ.setEmail(message);
+					verificationQ.save();
+
+					Verification.privateEmail(e, member);
+
+				} else {
+					e.getMessage().replyEmbeds(new TechEmbedBuilder("ERROR").text("Your message does not contain a email address").error().build()).complete();
+
+				}
+			} else if (array.length == 1 && check(message)) {
+				verificationQ.setTransactionId(message);
+				verificationQ.save();
+
+				Verification.privateTransactionId(e, member);
+
+			} else {
+				e.getMessage().replyEmbeds(new TechEmbedBuilder(member.getVerificationQ().getMarket().getName() + " Verification").text("Somethings is wrong please type `cancel` to cancel the verification and try again. If the new one fails as well please contact a staff member.").error().build()).complete();
+
+			}
+		} else {
+			e.getMessage().addReaction("👀").queue();
+		}
 	}
 
 	private boolean check(String s) {
 		if (s == null) { // checks if the String is null
 			return false;
-	}
-	int len = s.length();
-      for (int i = 0; i < len; i++) {
-		// checks whether the character is neither a letter nor a digit
-		// if it is neither a letter nor a digit then it will return false
-		if ((!Character.isLetterOrDigit(s.charAt(i)))) {
-			return false;
 		}
+		int len = s.length();
+		for (int i = 0; i < len; i++) {
+			// checks whether the character is neither a letter nor a digit
+			// if it is neither a letter nor a digit then it will return false
+			if ((!Character.isLetterOrDigit(s.charAt(i)))) {
+				return false;
+			}
+		}
+		return true;
 	}
-      return true;
 }
-}
+
