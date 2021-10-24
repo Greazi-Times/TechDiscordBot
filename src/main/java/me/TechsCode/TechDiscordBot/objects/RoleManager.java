258c3b36c590322793e0380666811dc2b67a2d94
Faab007NL <mail@fabiandingemans.nl>
2021-10-24 20:17:21 +0200

    Changed list to set

--- src/main/java/me/TechsCode/TechDiscordBot/objects/RoleManager.java
+++ src/main/java/me/TechsCode/TechDiscordBot/objects/RoleManager.java
@@ -5,20 +5,22 @@ import me.TechsCode.TechDiscordBot.mysql.Models.DbMember;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RoleManager {
     private final Member member;
-    private final List<Role> memberRoles;
+    private final Set<Role> memberRoles;
 
     public RoleManager(DbMember dbMember){
         this.member = dbMember.getDiscordMember();
-        this.memberRoles = dbMember.getDiscordMember().getRoles();
+        this.memberRoles = new HashSet<>(member.getRoles());
     }
 
     public RoleManager(Member member){
         this.member = member;
-        this.memberRoles = member.getRoles();
+        this.memberRoles = new HashSet<>(member.getRoles());
     }
 
     public void addRole(Role role){
@@ -34,7 +36,7 @@ public class RoleManager {
     }
 
     public void removeRoles(List<Role> roles){
-        memberRoles.removeAll(roles);
+        roles.forEach(memberRoles::remove);
     }
 
     public boolean hasRole(Role role){
