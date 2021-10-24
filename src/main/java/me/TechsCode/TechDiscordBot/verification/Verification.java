d9b2b1e94833aaaa29f8772f883b4d3e6c1a4907
Greazi <info@greazi.com>
2021-10-24 21:46:36 +0200

    Update Verification.java

--- src/main/java/me/TechsCode/TechDiscordBot/verification/Verification.java
+++ src/main/java/me/TechsCode/TechDiscordBot/verification/Verification.java
@@ -5,24 +5,34 @@ import me.TechsCode.TechDiscordBot.logs.VerificationLogs;
 import me.TechsCode.TechDiscordBot.mysql.Models.DbMarket;
 import me.TechsCode.TechDiscordBot.mysql.Models.DbMember;
 import me.TechsCode.TechDiscordBot.mysql.Models.DbVerfication;
+import me.TechsCode.TechDiscordBot.mysql.Models.Lists.VerficationMarketList;
 import me.TechsCode.TechDiscordBot.mysql.Models.VerficationQ;
+import me.TechsCode.TechDiscordBot.util.RoleAssigner;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
 import me.TechsCode.TechDiscordBot.verification.data.Lists.TransactionsList;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
+import java.util.stream.Collectors;
+
 public class Verification {
+
 
 	public static void Verification(Member m, DbMarket market, TextChannel channel){
 		User user = m.getUser();
 		String discordId = m.getId();
 
 		DbMember member = TechDiscordBot.getStorage().retrieveMemberByDiscordId(discordId);
-		//TODO Verfication
+
+		if(isVerified(m)) {
+			TechDiscordBot.getStorage().retrieveMemberByDiscordId(discordId).getVerificationQ().delete();
+			new TechEmbedBuilder("ERROR "+user.getAsMention()).error().text("Hello there,\nYou have previously verified yourself and your roles will be automatically updated as a result.\n\nThis update may take 15 to 20 minutes to complete.").sendTemporary(channel, 10);
+			return;
+		}
 		if(TechDiscordBot.getStorage().verficationQExists(member)) {
 			new TechEmbedBuilder("ERROR "+user.getAsMention()).error().text("You have already started a verification process please finish it first!").sendTemporary(channel, 10);
 			return;
@@ -38,29 +48,56 @@ public class Verification {
 		}
 	}
 
-	public static boolean isVerified(ButtonClickEvent e, TextChannel channel, TechEmbedBuilder errorMessage) {
-		Member member = e.getMember();
-		assert member != null;
+	public static boolean isVerified(Member e) {
+		String member = e.getUser().getId();
 
-		DbMember dbMember = TechDiscordBot.getStorage().retrieveMemberByDiscordId(member.getId());
-		DbVerfication existingVerification = TechDiscordBot.getStorage().retrieveMemberVerfication(dbMember);
-		if (existingVerification != null) {
-			errorMessage.text("You've already linked to your Marketplace Account and your roles will be updated automatically!").sendTemporary(channel, 10);
-			return true;
-		}
-		return false;
+		DbMember dbMember = TechDiscordBot.getStorage().retrieveMemberByDiscordId(member);
+		DbVerfication existingVerification = dbMember.getVerification();
+		if(existingVerification == null) return false;
+
+		VerficationMarketList markets = existingVerification.getMarkets();
+		DbMarket qMarket = dbMember.getVerificationQ().getMarket();
+
+		return markets.market(qMarket).stream().findFirst().isPresent();
 	}
 
-	public static void verify(PrivateMessageReceivedEvent e, int id, String message){
-		VerficationQ verficationQ = TechDiscordBot.getStorage().retrieveVerficationQById(id);
-		if(verficationQ == null)return;
+	public static void verify(PrivateMessageReceivedEvent e, DbMember member, Message message){
+		String marketList;
+		VerficationQ verficationQ = member.getVerificationQ();
+
+		if(verficationQ == null) return;
 		String email = verficationQ.getEmail();
 		String transactionId = verficationQ.getTransactionId();
+		String market = verficationQ.getMarket().getName();
 
-		TransactionsList transactions = TechDiscordBot.getPaypalAPI().searchTransaction(email, transactionId);
-		TechDiscordBot.log(transactions.toString());
+		TransactionsList check = TechDiscordBot.getPaypalAPI().verifySearchTransaction(email, transactionId, market);
+		if(check.isEmpty()){e.getMessage().replyEmbeds(new TechEmbedBuilder("No purchases found").text("The details you have given in do not exist in our system.\n\nIf you think this is a problem than please contact a @Senior-Support member inside <#311178000026566658>!").error().build()).queue(); verficationQ.delete(); return;}
 
-		VerificationLogs.log(new TechEmbedBuilder(e.getAuthor().getName() + "'s Verification Completed")
-				.success().text(e.getAuthor().getName() + " has successfully verified their SpigotMC Account!"));
+		TransactionsList transactions = TechDiscordBot.getPaypalAPI().emailSearchTransaction(email, market);
+		marketList = transactions.stream().map(transaction -> transaction.getPlugin().getName()).collect(Collectors.joining(", "));
+
+		message.editMessageEmbeds(new TechEmbedBuilder("Verfication Success").text("Plugins that have been found on your account:\n`" + marketList + "`").build()).queue();
+
+		VerificationLogs.log(new TechEmbedBuilder(e.getAuthor().getName() + "'s Verification Completed").success().text(e.getAuthor().getName() + " has successfully verified their SpigotMC Account!"));
+
+		verficationQ.delete();
+		RoleAssigner.addRoles(marketList, market, member);
+	}
+
+	public static void privateWhy(PrivateMessageReceivedEvent e){
+		e.getMessage().replyEmbeds(new TechEmbedBuilder("Why we need your email").text("Our new verification system uses paypal to retrieve purchases.\nWith this system we need your e-mail so we cen get your purchases.\n\n*Type* `cancel` *to cancel your verification*").build()).queue();
+	}
+
+	public static void privateCancel(PrivateMessageReceivedEvent e, DbMember member){
+		e.getMessage().replyEmbeds(new TechEmbedBuilder(member.getVerificationQ().getMarket().getName() + "Verification Canceled").text("Your verification has been canceled.").error().build()).queue();
+	}
+
+	public static void privateEmail(PrivateMessageReceivedEvent e, DbMember member){
+		e.getMessage().replyEmbeds(new TechEmbedBuilder(member.getVerificationQ().getMarket().getName() + " Verification").text("Now we need a transaction ID from one of your purchases.\n\n*Type* `transactionid` *to get a guide on how to get the ID*\n*Type* `cancel` *to cancel your verification*").build()).complete();
+	}
+
+	public static void privateTransactionId(PrivateMessageReceivedEvent e, DbMember member){
+		Message message = e.getMessage().replyEmbeds(new TechEmbedBuilder(member.getVerificationQ().getMarket().getName() + " Verification").text("Verifying purchase....").build()).complete();
+		verify(e, member, message);
 	}
 }
