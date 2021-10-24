0e7476ce6f78fc89b3aabc5ac78e2ad377105169
Greazi <info@greazi.com>
2021-10-24 21:46:15 +0200

    Update Resource.java

--- src/main/java/me/TechsCode/TechDiscordBot/mysql/Models/Resource.java
+++ src/main/java/me/TechsCode/TechDiscordBot/mysql/Models/Resource.java
@@ -2,6 +2,7 @@ package me.TechsCode.TechDiscordBot.mysql.Models;
 
 import me.TechsCode.TechDiscordBot.TechDiscordBot;
 import me.TechsCode.TechDiscordBot.mysql.Models.Lists.UpdateList;
+import net.dv8tion.jda.api.entities.Role;
 
 import java.util.Set;
 
@@ -10,17 +11,20 @@ public class Resource {
     private final int id;
     private final String name;
     private final int spigotId;
+    private final long discordRoleId;
 
-    public Resource(int id, String Name, int SpigotId) {
+    public Resource(int id, String Name, int SpigotId, long discordRoleId) {
         this.id = id;
         this.name = Name;
         this.spigotId = SpigotId;
+        this.discordRoleId= discordRoleId;
     }
 
-    public Resource(String Name, int SpigotId) {
+    public Resource(String Name, int SpigotId, long discordRoleId) {
         this.id = 0;
         this.name = Name;
         this.spigotId = SpigotId;
+        this.discordRoleId= discordRoleId;
     }
 
     public int getId() {
@@ -33,6 +37,14 @@ public class Resource {
 
     public int getSpigotId() {
         return spigotId;
+    }
+
+    public long getDiscordRoleId() {
+        return discordRoleId;
+    }
+
+    public Role getDiscordRole(){
+        return TechDiscordBot.getJDA().getRoleById(discordRoleId);
     }
 
     public void save(){
