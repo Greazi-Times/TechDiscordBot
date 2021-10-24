fe7d5f6a0aa075d51540320ddb1f493eb6b24134
Greazi <info@greazi.com>
2021-10-24 21:41:45 +0200

    Changed channel ID of the logs

--- src/main/java/me/TechsCode/TechDiscordBot/logs/VerificationLogs.java
+++ src/main/java/me/TechsCode/TechDiscordBot/logs/VerificationLogs.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 
 public class VerificationLogs {
 
-    private static final long CHANNEL_ID = 873361956361359361L;
+    private static final long CHANNEL_ID = 877248194109636651L;
 
     public static boolean log(String msg) {
         return sendChannel("Log", msg, null);
