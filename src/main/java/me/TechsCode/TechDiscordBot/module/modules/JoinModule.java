598a5b4b9b6acf68c8186f4fe85b3bda5e7f24c5
Greazi <info@greazi.com>
2021-11-01 20:08:34 +0100

    Added member add option for the database

--- src/main/java/me/TechsCode/TechDiscordBot/module/modules/JoinModule.java
+++ src/main/java/me/TechsCode/TechDiscordBot/module/modules/JoinModule.java
@@ -2,9 +2,11 @@ package me.TechsCode.TechDiscordBot.module.modules;
 
 import me.TechsCode.TechDiscordBot.TechDiscordBot;
 import me.TechsCode.TechDiscordBot.module.Module;
+import me.TechsCode.TechDiscordBot.mysql.Models.DbMember;
 import me.TechsCode.TechDiscordBot.objects.DefinedQuery;
 import me.TechsCode.TechDiscordBot.objects.Query;
 import me.TechsCode.TechDiscordBot.objects.Requirement;
+import me.TechsCode.TechDiscordBot.util.Roles;
 import me.TechsCode.TechDiscordBot.util.TechEmbedBuilder;
 import me.TechsCode.TechDiscordBot.verification.Verification;
 import net.dv8tion.jda.api.entities.Member;
@@ -38,6 +40,9 @@ public class JoinModule extends Module {
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {
         TechEmbedBuilder errorMessage = new TechEmbedBuilder("Error (" + e.getUser().getName() + ")").error();
         Member member = e.getMember();
+
+        DbMember dbMember = new DbMember(member.getId(), member.getUser().getName(), member.getTimeJoined().toEpochSecond(), false);
+        dbMember.save();
 
         new TechEmbedBuilder("Welcome to Tech's Plugin Support")
                 .text((Verification.isVerified(member) ? "Welcome to Tech's Plugin Support,\nIf you are looking for help, you must first verify your Spigot Account in <#695493411117072425>.\n\nAfter you have been verified, you will have access to the support channel(s)." : "Hello there,\nYou have previously verified yourself and your roles will be automatically updated as a result.\n\nThis update may take 10 to 15 minutes to complete.") + "\n\nThanks & Welcome,\nTechsCode & Team")
